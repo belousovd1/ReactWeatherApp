9ec73b390c2cdd663007b0f03330d3dda255bbfb
belousovd1 <db.wfha@gmail.com>
2021-11-04 14:57:49 -0400

    ReactWeatherApp- currently working input and get request(still issue with only noting previous search item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import background from "./bg.jpg"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const api = {
+  key: "5cda9dc4110bc6d50564701b260ba81e",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+const searchBox = document.querySelector('.search-boz')
+
+
+const App = () => {
+
+  const [city, setCity] = useState("")
+
+const getResults = (city) =>{
+    const getWeather = async () => {
+    try{
+      const response = await axios.get(`${api.base}weather?q=${city}&units=imperial&appid=${api.key}`)
+      console.log(response)
+      return response
+      } catch (err){
+      console.log(err)
+      }
+    }
+
+    return getWeather()
+  
+}
+
+  const keyHandler = (event) => {
+    if (event.code === "Enter"){
+      setCity(event.target.value)
+      console.log(city)
+      getResults(city)
+    } 
+  }
+
+  return(
+       <div className="app-wrap" style={{backgroundImage: `url(${background})`}}>
+        <header>
+            <input type="text"
+             autoComplete="off"
+             className="search-box"
+             onKeyDown={(e) => keyHandler(e)}
+             placeholder="Search for city...." />
+        </header>
+
+        <main>
+            <section className="location">
+                <div className="city">city</div>
+                <div className="date">date</div>
+            </section>
+            <div className="current">
+                <div className="temp">number, <span>unit</span></div>
+                <div className="weather">weather</div>
+                <div className="hi-low">hi-low</div>
+            </div>
+        </main>
+    </div>  
+  )
 }
 
 export default App;
