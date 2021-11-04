a90abc5d4d537dae1e45fae20cc8634aa9791f58
belousovd1 <db.wfha@gmail.com>
2021-11-04 15:53:44 -0400

    It works boys and girls

--- src/App.js
+++ src/App.js
@@ -8,53 +8,87 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+function dateBuilder(d){
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "Semptember", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}` ;
+}
+
 const App = () => {
 
-let city = ""
+  const [city, setCity] = useState("City")
+  const [country, setCountry] = useState("Country")
+  const [date, setDate] = useState("Date")
+  const [temperature, setTemperature] = useState("Temperature")
+  const [weather, setWeather] = useState("Weather")
+  const [HiTemp, setHiTemp] = useState("Hi")
+  const [LowTemp, setLowTemp] = useState("Lo")
 
-const getResults = (city) =>{
+  let input = ""
+
+  const getResults = (city) => {
     const getWeather = async () => {
-    try{
-      const response = await axios.get(`${api.base}weather?q=${city}&units=imperial&appid=${api.key}`)
-      console.log(response)
-      return response
-      } catch (err){
-      console.log(err)
+      try {
+        const response = await axios.get(`${api.base}weather?q=${city}&units=imperial&appid=${api.key}`)
+        console.log(response.data)
+        return response.data
+      } catch (err) {
+        console.log(err)
       }
     }
     return getWeather()
-}
-
-  const keyHandler = (event) => {
-    if (event.code === "Enter"){
-      city = event.target.value
-      console.log(city)
-      getResults(city)
-    } 
   }
 
-  return(
-       <div className="app-wrap" style={{backgroundImage: `url(${background})`}}>
-        <header>
-            <input type="text"
-             autoComplete="off"
-             className="search-box"
-             onKeyDown={(e) => keyHandler(e)}
-             placeholder="Search for city...." />
-        </header>
+  const updateInfo = (weatherJSON) =>{ 
+    console.log(weatherJSON, "weatherJSON")
+    setCountry(weatherJSON.sys.country)
+    setCity(weatherJSON.name)
+    let now = new Date()
+    setDate(dateBuilder(now))
+    setTemperature(weatherJSON.main.temp)
+    setWeather(weatherJSON.weather[0].main)
+    setHiTemp(weatherJSON.main.temp_max)
+    setLowTemp(weatherJSON.main.temp_min)
+  }
 
-        <main>
-            <section className="location">
-                <div className="city">city</div>
-                <div className="date">date</div>
-            </section>
-            <div className="current">
-                <div className="temp">number, <span>unit</span></div>
-                <div className="weather">weather</div>
-                <div className="hi-low">hi-low</div>
-            </div>
-        </main>
-    </div>  
+  const keyHandler = async (event) => {
+    if (event.code === "Enter") {
+      input = event.target.value
+      console.log(input)
+      const weather = await getResults(input)
+      updateInfo(weather)
+    }
+  }
+
+  return (
+    <div className="app-wrap" style={{ backgroundImage: `url(${background})` }}>
+      <header>
+        <input type="text"
+          autoComplete="off"
+          className="search-box"
+          onKeyDown={(e) => keyHandler(e)}
+          placeholder="Search for city...." />
+      </header>
+
+      <main>
+        <section className="location">
+          <div className="city">{city}, {country}</div>
+          <div className="date">{date}</div>
+        </section>
+        <div className="current">
+          <div className="temp">{temperature}, <span>°f</span></div>
+          <div className="weather">{weather}</div>
+          <div className="hi-low">{HiTemp}-{LowTemp}</div>
+        </div>
+      </main>
+    </div>
   )
 }
 
